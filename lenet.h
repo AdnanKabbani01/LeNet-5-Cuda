6d026685fc37d2b1b8f94e7201cfa706a53f27d4
Adnan Kabbani <adnan.kabbani01@lau.edu>
2024-12-04 14:24:54 +0200

    .h cuda committtt

--- lenet.h
+++ lenet.h
@@ -4,9 +4,39 @@
 extern "C" {
 #endif
 
+// Network parameters
+#define LENGTH_KERNEL    5
+#define LENGTH_FEATURE0  32
+#define LENGTH_FEATURE1  (LENGTH_FEATURE0 - LENGTH_KERNEL + 1)
+#define LENGTH_FEATURE2  (LENGTH_FEATURE1 >> 1)
+#define LENGTH_FEATURE3  (LENGTH_FEATURE2 - LENGTH_KERNEL + 1)
+#define LENGTH_FEATURE4  (LENGTH_FEATURE3 >> 1)
+#define LENGTH_FEATURE5  (LENGTH_FEATURE4 - LENGTH_KERNEL + 1)
+
+#define INPUT            1
+#define LAYER1           6
+#define LAYER2           6
+#define LAYER3           16
+#define LAYER4           16
+#define LAYER5           120
+#define OUTPUT           10
+
+#define INPUT_CHANNELS   1
+#define INPUT_SIZE       32
+#define LAYER1_CHANNELS  6
+#define LAYER1_SIZE      28
+#define LAYER2_CHANNELS  16
+#define LAYER2_SIZE      14
+#define LAYER4_CHANNELS  120
+#define LAYER4_SIZE      1
+#define KERNEL_SIZE      5
+
+#define ALPHA            0.5
+#define PADDING          2
+
 // Type definitions
-typedef unsigned char uint8; // Define uint8
-typedef uint8 image[28][28]; // Define the image type
+typedef unsigned char uint8;
+typedef uint8 image[28][28];
 
 // Structure definitions
 typedef struct LeNet5
@@ -42,33 +72,3 @@ void Initial(LeNet5 *lenet);
 #ifdef __cplusplus
 }
 #endif
-
-// Network parameters
-#define LENGTH_KERNEL    5
-#define LENGTH_FEATURE0  32
-#define LENGTH_FEATURE1  (LENGTH_FEATURE0 - LENGTH_KERNEL + 1)
-#define LENGTH_FEATURE2  (LENGTH_FEATURE1 >> 1)
-#define LENGTH_FEATURE3  (LENGTH_FEATURE2 - LENGTH_KERNEL + 1)
-#define LENGTH_FEATURE4  (LENGTH_FEATURE3 >> 1)
-#define LENGTH_FEATURE5  (LENGTH_FEATURE4 - LENGTH_KERNEL + 1)
-
-#define INPUT            1
-#define LAYER1           6
-#define LAYER2           6
-#define LAYER3           16
-#define LAYER4           16
-#define LAYER5           120
-#define OUTPUT           10
-
-#define INPUT_CHANNELS   1
-#define INPUT_SIZE       32
-#define LAYER1_CHANNELS  6
-#define LAYER1_SIZE      28
-#define LAYER2_CHANNELS  16
-#define LAYER2_SIZE      14
-#define LAYER4_CHANNELS  120
-#define LAYER4_SIZE      1
-#define KERNEL_SIZE      5
-
-#define ALPHA            0.5
-#define PADDING          2
